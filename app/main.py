3c4f8fcc11762951af9619727e56c4177c82919c
fwt11 <wutong.feng@139.com>
2023-04-04 20:59:01 +0800

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -8,11 +8,13 @@ def main():
 
     # Uncomment this block to pass the first stage
     #
-    # command = sys.argv[3]
-    # args = sys.argv[4:]
-    #
-    # completed_process = subprocess.run([command, *args], capture_output=True)
-    # print(completed_process.stdout.decode("utf-8"))
+
+    print(sys.argv)
+    command = sys.argv[3]
+    args = sys.argv[4:]
+    
+    completed_process = subprocess.run([command, *args], capture_output=True)
+    print(completed_process.stdout.decode("utf-8"))
 
 
 if __name__ == "__main__":
