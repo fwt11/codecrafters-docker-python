4d279ce4ec0a4226034efc5c8d477cd3b590f875
fwt11 <wutong.feng@139.com>
2023-04-04 21:24:08 +0800

    wireup stdout & stderr

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ def main():
     args = sys.argv[4:]
     
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    print(completed_process.stdout.decode("utf-8"), file=sys.stdout, end='')
+    print(completed_process.stderr.decode("utf-8"), file=sys.stderr, end='')
 
 
 if __name__ == "__main__":
