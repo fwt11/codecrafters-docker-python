c9030a8a59d9624fb09c9abfac7be7d8faae261c
fwt11 <wutong.feng@139.com>
2023-04-05 21:14:58 +0800

    isolation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import subprocess
 import sys
+import os
+import tempfile
+import shutil
 
 
 def main():
@@ -12,12 +15,22 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"), file=sys.stdout, end='')
-    print(completed_process.stderr.decode("utf-8"), file=sys.stderr, end='')
+
+
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        print(temp_dir)
+        os.chdir(temp_dir)
+        os.makedirs("usr/bin")
+        os.makedirs("usr/local/bin")
+        shutil.copy("/usr/bin/ls", "usr/bin/")
+        shutil.copy("/usr/local/bin/docker-explorer", "usr/local/bin/")
+        os.chroot(".")
+        completed_process = subprocess.run([command, *args], capture_output=True)
+        print(completed_process.stdout.decode("utf-8"), file=sys.stdout, end='')
+        print(completed_process.stderr.decode("utf-8"), file=sys.stderr, end='')
+
     sys.exit(completed_process.returncode)
-
-
 
 
 if __name__ == "__main__":
