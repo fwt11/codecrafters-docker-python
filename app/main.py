0d09b7510b5f3ba7e19478f896a7925cb9ed8a07
fwt11 <wutong.feng@139.com>
2023-04-14 07:27:44 +0800

    pull layer

--- app/main.py
+++ app/main.py
@@ -4,9 +4,79 @@ import os
 import tempfile
 import shutil
 from ctypes import *
-
+from urllib import request
+from urllib.error import HTTPError
+import json
+from pprint import pprint
+import tarfile
 
 CLONE_NEWPID = 0x20000000
+
+
+
+
+def get_manifest(name, reference):
+    url = f'https://registry.hub.docker.com/v2/{name}/manifests/{reference}'
+    try:
+        r = request.urlopen(url)
+        j = json.loads(r.read())
+        return j
+    except HTTPError as http_error:
+        if http_error.reason == 'Unauthorized': 
+            req = request.Request(url)
+            token = get_token(name, "pull")
+            req.add_header("Authorization", f"Bearer {token}")
+            req.add_header("Accept", "application/vnd.oci.image.index.v1+json")
+            r = request.urlopen(req)
+            print(r)
+            j = json.loads(r.read())
+            return j
+        else:
+            print(f"HTTPError: {http_error}")
+    except Exception as e:
+        print(f"Exception: {e}")
+
+    return j
+
+
+def pull_layer(name, digest):
+    filename = digest.split(":")[1] + ".tar"
+    url = f'https://registry.hub.docker.com/v2/{name}/blobs/{digest}'
+    print(f"url: {url}")
+    try:
+        r = request.urlopen(url)
+        print(r.getcode())
+        if r.getcode() == 307:
+            url = r.getheader('Location')
+            print(url)
+            r = request.urlopen(url)
+            with open(filename, 'bw') as f:
+                f.write(r.read())
+            tar = tarfile.open(filename)
+            tar.extractall()
+    except HTTPError as http_error:
+        if http_error.reason == 'Unauthorized': 
+            req = request.Request(url)
+            token = get_token(name, "pull")
+            req.add_header("Authorization", f"Bearer {token}")
+            r = request.urlopen(req)
+            with open(filename, 'bw') as f:
+                f.write(r.read())
+            
+            tar = tarfile.open(filename)
+            tar.extractall()
+    except Exception as e:
+        print(e)
+
+
+
+def get_token(name, action):
+
+    name = name.replace("/", "%2F")
+    #r = request.urlopen(f"https://auth.docker.io/token?scope=repository%3A{name}%3A{action}&service=registry.docker.io")
+    r = request.urlopen(f"https://auth.docker.io/token?scope=repository:{name}:{action}&service=registry.docker.io")
+    j = json.loads(r.read())
+    return j['token']
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,6 +84,9 @@ def main():
 
     # Uncomment this block to pass the first stage
     #
+    image = sys.argv[2]
+    image_name, image_ref = image.split(":")
+    image_name = f"library/{image_name}"
 
     command = sys.argv[3]
     args = sys.argv[4:]
@@ -26,11 +99,26 @@ def main():
 
     with tempfile.TemporaryDirectory() as temp_dir:
         os.chdir(temp_dir)
-
         for d in dirs:
             os.makedirs(d)
     
         shutil.copy("/usr/local/bin/docker-explorer", "usr/local/bin/")
+
+        manifests = get_manifest(image_name, image_ref)
+        if manifests["schemaVersion"] == 2:
+            layers = manifests["manifests"]
+            for layer in layers:
+                digest = layer["digest"]
+                print(f"pulling {digest}")
+                pull_layer(image_name, digest)
+        else:
+            layers = manifests["fsLayers"]
+            for layer in layers:
+                digest = layer["blobSum"]
+                print(f"pulling {digest}")
+                pull_layer(image_name, digest)
+
+
 
         os.chroot(".")
         completed_process = subprocess.run([command, *args], capture_output=True)
@@ -40,5 +128,9 @@ def main():
     sys.exit(completed_process.returncode)
 
 
+
+
 if __name__ == "__main__":
     main()
+
+
